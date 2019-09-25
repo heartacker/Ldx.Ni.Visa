2365dc8250cbe6d833264d7abc8e6cf505610019
heartcker <1014919474@qq.com>
2019-09-26 00:14:15 +0800

    添加项目文件。

---
+++ Ldx.Ni.Visa/Info.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using NationalInstruments.VisaNS;
+using Ivi.Visa; 
+namespace Ldx.Ni.Visa
+{
+    public static class Info
+    {
+        public static string Text { get=>"NI.Visa Mask";}
+    }
+}
