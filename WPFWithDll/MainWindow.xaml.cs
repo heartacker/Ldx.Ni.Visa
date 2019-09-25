337726fd1192f0e9b8f0259f29e5d8b9ac3bc765
heartcker <1014919474@qq.com>
2019-09-26 01:12:07 +0800

    原来如此， 但是如何件dll 自动打包到nuget里面呢？

---
+++ WPFWithDll/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ivi.Visa;
+using Ldx.Ni.Visa;
+using NationalInstruments.VisaNS;
+
+namespace WPFWithDll
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(typeof(NationalInstruments.VisaNS.IMessageBasedSession).FullName);
+        }
+    }
+}
